67c59615f552b1d9e246aaf60e689e6eac472c34
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-03-11 17:05:42 +0900

    210311 커밋

--- WinformApp/ExcerciseWinApp/DigitalAlarmClockApp/FrmAlarm.Designer.cs
+++ WinformApp/ExcerciseWinApp/DigitalAlarmClockApp/FrmAlarm.Designer.cs
@@ -29,20 +29,22 @@ namespace DigitalAlarmClockApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.TabClock = new System.Windows.Forms.TabControl();
             this.TapSetAlarm = new System.Windows.Forms.TabPage();
-            this.TapDigitalClock = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
+            this.BtnRelease = new System.Windows.Forms.Button();
+            this.BtnSet = new System.Windows.Forms.Button();
+            this.DtpAlarmTime = new System.Windows.Forms.DateTimePicker();
+            this.DtpAlarmDate = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TapDigitalClock = new System.Windows.Forms.TabPage();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.LblDate = new System.Windows.Forms.Label();
+            this.LblTime = new System.Windows.Forms.Label();
+            this.LblAlarm = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MyTimer = new System.Windows.Forms.Timer(this.components);
             this.TabClock.SuspendLayout();
             this.TapSetAlarm.SuspendLayout();
             this.TapDigitalClock.SuspendLayout();
@@ -61,10 +63,10 @@ namespace DigitalAlarmClockApp
             // 
             // TapSetAlarm
             // 
-            this.TapSetAlarm.Controls.Add(this.button2);
-            this.TapSetAlarm.Controls.Add(this.button1);
-            this.TapSetAlarm.Controls.Add(this.dateTimePicker2);
-            this.TapSetAlarm.Controls.Add(this.dateTimePicker1);
+            this.TapSetAlarm.Controls.Add(this.BtnRelease);
+            this.TapSetAlarm.Controls.Add(this.BtnSet);
+            this.TapSetAlarm.Controls.Add(this.DtpAlarmTime);
+            this.TapSetAlarm.Controls.Add(this.DtpAlarmDate);
             this.TapSetAlarm.Controls.Add(this.label2);
             this.TapSetAlarm.Controls.Add(this.label1);
             this.TapSetAlarm.Location = new System.Drawing.Point(4, 22);
@@ -75,27 +77,41 @@ namespace DigitalAlarmClockApp
             this.TapSetAlarm.Text = "알람설정";
             this.TapSetAlarm.UseVisualStyleBackColor = true;
             // 
-            // TapDigitalClock
+            // BtnRelease
             // 
-            this.TapDigitalClock.Controls.Add(this.groupBox1);
-            this.TapDigitalClock.Controls.Add(this.label4);
-            this.TapDigitalClock.Controls.Add(this.label3);
-            this.TapDigitalClock.Location = new System.Drawing.Point(4, 22);
-            this.TapDigitalClock.Name = "TapDigitalClock";
-            this.TapDigitalClock.Padding = new System.Windows.Forms.Padding(3);
-            this.TapDigitalClock.Size = new System.Drawing.Size(434, 298);
-            this.TapDigitalClock.TabIndex = 1;
-            this.TapDigitalClock.Text = "디지털 시계";
-            this.TapDigitalClock.UseVisualStyleBackColor = true;
+            this.BtnRelease.Location = new System.Drawing.Point(341, 145);
+            this.BtnRelease.Name = "BtnRelease";
+            this.BtnRelease.Size = new System.Drawing.Size(75, 23);
+            this.BtnRelease.TabIndex = 3;
+            this.BtnRelease.Text = "해제";
+            this.BtnRelease.UseVisualStyleBackColor = true;
+            this.BtnRelease.Click += new System.EventHandler(this.BtnRelease_Click);
             // 
-            // label1
+            // BtnSet
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "날짜 설정";
+            this.BtnSet.Location = new System.Drawing.Point(260, 145);
+            this.BtnSet.Name = "BtnSet";
+            this.BtnSet.Size = new System.Drawing.Size(75, 23);
+            this.BtnSet.TabIndex = 3;
+            this.BtnSet.Text = "설정";
+            this.BtnSet.UseVisualStyleBackColor = true;
+            this.BtnSet.Click += new System.EventHandler(this.BtnSet_Click);
+            // 
+            // DtpAlarmTime
+            // 
+            this.DtpAlarmTime.CustomFormat = "hh:mm:ss";
+            this.DtpAlarmTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DtpAlarmTime.Location = new System.Drawing.Point(27, 115);
+            this.DtpAlarmTime.Name = "DtpAlarmTime";
+            this.DtpAlarmTime.Size = new System.Drawing.Size(200, 21);
+            this.DtpAlarmTime.TabIndex = 2;
+            // 
+            // DtpAlarmDate
+            // 
+            this.DtpAlarmDate.Location = new System.Drawing.Point(27, 51);
+            this.DtpAlarmDate.Name = "DtpAlarmDate";
+            this.DtpAlarmDate.Size = new System.Drawing.Size(200, 21);
+            this.DtpAlarmDate.TabIndex = 2;
             // 
             // label2
             // 
@@ -106,85 +122,75 @@ namespace DigitalAlarmClockApp
             this.label2.TabIndex = 1;
             this.label2.Text = "시간 설정";
             // 
-            // dateTimePicker1
+            // label1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(27, 51);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
-            this.dateTimePicker1.TabIndex = 2;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "날짜 설정";
             // 
-            // dateTimePicker2
+            // TapDigitalClock
             // 
-            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dateTimePicker2.Location = new System.Drawing.Point(27, 115);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 21);
-            this.dateTimePicker2.TabIndex = 2;
+            this.TapDigitalClock.Controls.Add(this.groupBox1);
+            this.TapDigitalClock.Controls.Add(this.LblAlarm);
+            this.TapDigitalClock.Controls.Add(this.label3);
+            this.TapDigitalClock.Location = new System.Drawing.Point(4, 22);
+            this.TapDigitalClock.Name = "TapDigitalClock";
+            this.TapDigitalClock.Padding = new System.Windows.Forms.Padding(3);
+            this.TapDigitalClock.Size = new System.Drawing.Size(434, 298);
+            this.TapDigitalClock.TabIndex = 1;
+            this.TapDigitalClock.Text = "디지털 시계";
+            this.TapDigitalClock.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // groupBox1
             // 
-            this.button1.Location = new System.Drawing.Point(260, 145);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "설정";
-            this.button1.UseVisualStyleBackColor = true;
+            this.groupBox1.Controls.Add(this.LblDate);
+            this.groupBox1.Controls.Add(this.LblTime);
+            this.groupBox1.Location = new System.Drawing.Point(44, 112);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 169);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "현재시간";
             // 
-            // button2
+            // LblDate
             // 
-            this.button2.Location = new System.Drawing.Point(341, 145);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "해제";
-            this.button2.UseVisualStyleBackColor = true;
+            this.LblDate.AutoSize = true;
+            this.LblDate.Location = new System.Drawing.Point(6, 33);
+            this.LblDate.Name = "LblDate";
+            this.LblDate.Size = new System.Drawing.Size(30, 12);
+            this.LblDate.TabIndex = 3;
+            this.LblDate.Text = "Date";
+            // 
+            // LblTime
+            // 
+            this.LblTime.AutoSize = true;
+            this.LblTime.Font = new System.Drawing.Font("나눔고딕코딩", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.LblTime.Location = new System.Drawing.Point(6, 78);
+            this.LblTime.Name = "LblTime";
+            this.LblTime.Size = new System.Drawing.Size(79, 32);
+            this.LblTime.TabIndex = 4;
+            this.LblTime.Text = "Time";
+            // 
+            // LblAlarm
+            // 
+            this.LblAlarm.AutoSize = true;
+            this.LblAlarm.Location = new System.Drawing.Point(42, 76);
+            this.LblAlarm.Name = "LblAlarm";
+            this.LblAlarm.Size = new System.Drawing.Size(50, 12);
+            this.LblAlarm.TabIndex = 1;
+            this.LblAlarm.Text = "Alarm : ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(42, 44);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.Size = new System.Drawing.Size(57, 12);
             this.label3.TabIndex = 0;
-            this.label3.Text = "label3";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(42, 76);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 12);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "label4";
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Location = new System.Drawing.Point(44, 112);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(356, 169);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 33);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 12);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "label5";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 78);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 12);
-            this.label6.TabIndex = 4;
-            this.label6.Text = "label6";
+            this.label3.Text = "알람 설정";
             // 
             // FrmAlarm
             // 
@@ -197,6 +203,7 @@ namespace DigitalAlarmClockApp
             this.Name = "FrmAlarm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "알람시계";
+            this.Load += new System.EventHandler(this.FrmAlarm_Load);
             this.TabClock.ResumeLayout(false);
             this.TapSetAlarm.ResumeLayout(false);
             this.TapSetAlarm.PerformLayout();
@@ -212,18 +219,19 @@ namespace DigitalAlarmClockApp
 
         private System.Windows.Forms.TabControl TabClock;
         private System.Windows.Forms.TabPage TapSetAlarm;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button BtnRelease;
+        private System.Windows.Forms.Button BtnSet;
+        private System.Windows.Forms.DateTimePicker DtpAlarmTime;
+        private System.Windows.Forms.DateTimePicker DtpAlarmDate;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TabPage TapDigitalClock;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LblDate;
+        private System.Windows.Forms.Label LblTime;
+        private System.Windows.Forms.Label LblAlarm;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer MyTimer;
     }
 }
 
---
+++ WinformApp/ExcerciseWinApp/SimpleGraphicEditor/FrmMain.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleGraphicEditor
+{
+    enum DrawMode
+    {
+        LiNE,
+        RECTANGLE,
+        CIRCLE,
+        CURVED_LINE
+    }
+
+    public partial class FrmMain : Form
+    {
+        // 멤버변수들
+        private DrawMode mode;
+        private Graphics g;
+        private Pen pen;
+        private Pen eraser;
+        private Point startP, endP, currP, prevP; 
+        //
+
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            StsCurrent.Text = "";
+
+            g = CreateGraphics();
+            this.pen = new Pen(Color.Black, 2);
+            StsCurrent.Text = "Line Mode";
+            this.mode = DrawMode.LiNE;
+            this.BackColor = Color.White;
+            this.eraser = new Pen(Color.White, 2);
+
+        }
+
+        private void TlmLine_Click(object sender, EventArgs e)
+        {
+            this.mode = DrawMode.LiNE;
+            StsCurrent.Text = "Line Mode";
+
+        }
+
+        private void TlmRectangle_Click(object sender, EventArgs e)
+        {
+            this.mode = DrawMode.RECTANGLE;
+            StsCurrent.Text = "RECTANGLE Mode";
+        }
+
+        private void FrmMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.startP = new Point(e.X,e.Y);
+            this.currP =  this.prevP = this.startP;
+        }
+
+
+        // 마우스 옮길때 마다 발생
+
+        private void FrmMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+
+            this.prevP = this.currP;
+            this.currP = new Point(e.X, e.Y);
+
+            switch (this.mode)
+            {
+                case DrawMode.LiNE:
+                    g.DrawLine(this.eraser, this.startP, this.prevP);
+                    g.DrawLine(this.pen, this.startP, this.currP);
+                    break;
+                case DrawMode.RECTANGLE:
+                    g.DrawRectangle(this.eraser, 
+                        new Rectangle(startP, new Size(prevP.X - startP.X, prevP.Y - startP.Y)));
+                    g.DrawRectangle(this.pen,
+                        new Rectangle(startP, new Size(currP.X - startP.X, currP.Y - startP.Y)));
+                    break;
+                case DrawMode.CIRCLE:
+                    g.DrawEllipse(this.eraser,
+                        new Rectangle(startP, new Size(prevP.X - startP.X, prevP.Y - startP.Y)));
+                    g.DrawEllipse(this.pen,
+                        new Rectangle(startP, new Size(currP.X - startP.X, currP.Y - startP.Y)));
+                    break;
+                case DrawMode.CURVED_LINE:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void FrmMain_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.endP = new Point(e.X, e.Y);
+            switch (this.mode)
+            {
+                case DrawMode.LiNE:
+                    g.DrawLine(this.pen, this.startP, this.endP);
+                    break;
+                case DrawMode.RECTANGLE:
+                    g.DrawRectangle(this.pen,
+                        new Rectangle(startP, new Size(endP.X - startP.X, endP.Y - startP.Y)));
+                    break;
+                case DrawMode.CIRCLE:
+                    g.DrawEllipse(this.pen,
+                        new Rectangle(startP, new Size(endP.X - startP.X, endP.Y - startP.Y)));
+                    break;
+                case DrawMode.CURVED_LINE:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void TmlCircle_Click(object sender, EventArgs e)
+        {
+            this.mode = DrawMode.CIRCLE;
+            StsCurrent.Text = "CIRCLE Mode";
+        }
+
+        private void TlmCurvedLine_Click(object sender, EventArgs e)
+        {
+            this.mode = DrawMode.CURVED_LINE;
+            StsCurrent.Text = "CURVED_LINE Mode";
+        }
+
+        private void TlmColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.pen.Color = colorDialog.Color;
+            }
+        }
+    }
+}
