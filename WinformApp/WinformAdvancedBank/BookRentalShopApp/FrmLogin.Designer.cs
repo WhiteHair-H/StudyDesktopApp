db3142142cedb68781b808aca331ee55bc7e6743
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-03-16 17:27:48 +0900

    화요일 커밋

---
+++ WinformApp/WinformAdvancedBank/BookRentalShopApp/FrmLogin.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace BookRentalShopApp
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metro = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.TxtUserId = new MetroFramework.Controls.MetroTextBox();
+            this.TxtPassword = new MetroFramework.Controls.MetroTextBox();
+            this.BtnLogin = new MetroFramework.Controls.MetroButton();
+            this.BtnCancel = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metro
+            // 
+            this.metro.AutoSize = true;
+            this.metro.Location = new System.Drawing.Point(53, 75);
+            this.metro.Name = "metro";
+            this.metro.Size = new System.Drawing.Size(46, 19);
+            this.metro.TabIndex = 0;
+            this.metro.Text = "UserId";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(33, 105);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(66, 19);
+            this.metroLabel2.TabIndex = 1;
+            this.metroLabel2.Text = "PassWord";
+            // 
+            // TxtUserId
+            // 
+            // 
+            // 
+            // 
+            this.TxtUserId.CustomButton.Image = null;
+            this.TxtUserId.CustomButton.Location = new System.Drawing.Point(157, 1);
+            this.TxtUserId.CustomButton.Name = "";
+            this.TxtUserId.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.TxtUserId.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TxtUserId.CustomButton.TabIndex = 1;
+            this.TxtUserId.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TxtUserId.CustomButton.UseSelectable = true;
+            this.TxtUserId.CustomButton.Visible = false;
+            this.TxtUserId.Lines = new string[0];
+            this.TxtUserId.Location = new System.Drawing.Point(122, 75);
+            this.TxtUserId.MaxLength = 20;
+            this.TxtUserId.Name = "TxtUserId";
+            this.TxtUserId.PasswordChar = '\0';
+            this.TxtUserId.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TxtUserId.SelectedText = "";
+            this.TxtUserId.SelectionLength = 0;
+            this.TxtUserId.SelectionStart = 0;
+            this.TxtUserId.ShortcutsEnabled = true;
+            this.TxtUserId.Size = new System.Drawing.Size(179, 23);
+            this.TxtUserId.TabIndex = 1;
+            this.TxtUserId.UseSelectable = true;
+            this.TxtUserId.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TxtUserId.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.TxtUserId.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtUserId_KeyPress);
+            // 
+            // TxtPassword
+            // 
+            // 
+            // 
+            // 
+            this.TxtPassword.CustomButton.Image = null;
+            this.TxtPassword.CustomButton.Location = new System.Drawing.Point(157, 1);
+            this.TxtPassword.CustomButton.Name = "";
+            this.TxtPassword.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.TxtPassword.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TxtPassword.CustomButton.TabIndex = 1;
+            this.TxtPassword.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TxtPassword.CustomButton.UseSelectable = true;
+            this.TxtPassword.CustomButton.Visible = false;
+            this.TxtPassword.Lines = new string[0];
+            this.TxtPassword.Location = new System.Drawing.Point(122, 105);
+            this.TxtPassword.MaxLength = 32767;
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '●';
+            this.TxtPassword.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TxtPassword.SelectedText = "";
+            this.TxtPassword.SelectionLength = 0;
+            this.TxtPassword.SelectionStart = 0;
+            this.TxtPassword.ShortcutsEnabled = true;
+            this.TxtPassword.Size = new System.Drawing.Size(179, 23);
+            this.TxtPassword.TabIndex = 2;
+            this.TxtPassword.UseSelectable = true;
+            this.TxtPassword.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TxtPassword.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.TxtPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtPassword_KeyPress);
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(68, 146);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(79, 31);
+            this.BtnLogin.TabIndex = 3;
+            this.BtnLogin.Text = "로그인";
+            this.BtnLogin.UseSelectable = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(186, 146);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(79, 31);
+            this.BtnCancel.TabIndex = 4;
+            this.BtnCancel.Text = "취소";
+            this.BtnCancel.UseSelectable = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(321, 210);
+            this.ControlBox = false;
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtUserId);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metro);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmLogin";
+            this.Resizable = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "로그인";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel metro;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroTextBox TxtUserId;
+        private MetroFramework.Controls.MetroTextBox TxtPassword;
+        private MetroFramework.Controls.MetroButton BtnLogin;
+        private MetroFramework.Controls.MetroButton BtnCancel;
+    }
+}
---
+++ WinformApp/WinformAdvancedBank/BookRentalShopApp/Frmmain.cs
@@ -0,0 +1,32 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookRentalShopApp
+{
+    public partial class Frmmain : MetroForm
+    {
+        public Frmmain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Frmmain_Shown(object sender, EventArgs e)
+        {
+            FrmLogin frm = new FrmLogin();
+            frm.ShowDialog();
+        }
+    }
+}
